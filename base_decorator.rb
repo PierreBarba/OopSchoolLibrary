e756cad890a3830e5642e72c91fcb87e2f95ef28
Pierre Barba <99061965+PierreBarba@users.noreply.github.com>
2023-03-14 12:57:04 -0600

    create file & class base_decorator

---
+++ base_decorator.rb
@@ -0,0 +1,12 @@
+requiere_relative 'nameable'
+
+class Base_decorator < Nameable
+    def initialize(nameable)
+        super()
+        @nameable = nameable
+      end
+    
+      def correct_name
+        @nameable.correct_name
+      end
+end
