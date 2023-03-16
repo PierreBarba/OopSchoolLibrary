c2dfb3f859249a987282faaf55b04645a8f6c04c
Pierre Barba <99061965+PierreBarba@users.noreply.github.com>
2023-03-15 23:39:04 -0600

    refactor class Rental

--- rental.rb
+++ rental.rb
@@ -1,5 +1,5 @@
-requiere_relative 'person'
-requiere_relative 'Book'
+require_relative 'book'
+require_relative 'person'
 
 class Rental
   attr_accessor :date
@@ -8,9 +8,16 @@ class Rental
   def initialize(date, book, person)
     @date = date
     @book = book
-    book.rentals << self
-
     @person = person
-    person.rentals << self
+  end
+
+  def book=(book)
+    @book = book
+    book.rentals.push(self) unless book.rentals.include?(self)
+  end
+
+  def person=(person)
+    @person = person
+    person.rentals.push(self) unless person.rentals.include?(self)
   end
 end
