01ca49a87cc4e42458a17ff61a283397cfcb208f
Ivan Martinez von Halle <ivanmvh@gmail.com>
2023-03-21 09:40:59 -0500

    Fix rubocop - linters

--- main.rb
+++ main.rb
@@ -12,23 +12,23 @@ def main
   puts 'Thanks for using this app!'
 end
 
-  def selected_menu_option
-    puts '-------> SCHOOL LIBRARY APP <-------'
-    puts "\nPlease choose an option by entering a number: "
-    puts '1 - List all books.'
-    puts '2 - List all users.'
-    puts '3 - Create a person,'
-    puts '4 - Create a book.'
-    puts '5 - Create a rental.'
-    puts '6 - List all rentals for a given person id.'
-    puts '7 - Exit'
+def selected_menu_option
+  puts '-------> SCHOOL LIBRARY APP <-------'
+  puts "\nPlease choose an option by entering a number: "
+  puts '1 - List all books.'
+  puts '2 - List all users.'
+  puts '3 - Create a person,'
+  puts '4 - Create a book.'
+  puts '5 - Create a rental.'
+  puts '6 - List all rentals for a given person id.'
+  puts '7 - Exit'
 
-    get_option_selected(1, 7)
-  end
+  get_option_selected(1, 7)
+end
 
-    def wait_user
-    puts 'Press any key to continue...'
-    gets.chomp
-    puts ''
-  end
+def wait_user
+  puts 'Press any key to continue...'
+  gets.chomp
+  puts ''
+end
 main
