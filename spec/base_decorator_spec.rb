538f77dfed6ed180882cbc2158fbbfae14d4e5f0
Pierre Barba <99061965+PierreBarba@users.noreply.github.com>
2023-03-22 17:57:19 -0600

    base decorator

--- spec/base_decorator_spec.rb
+++ spec/base_decorator_spec.rb
@@ -1,17 +1,20 @@
-require_relative '../base_decorator'describe BaseDecorator do
+require_relative '../base_decorator'
+describe BaseDecorator do
   context 'When testing BaseDecorator Class' do
     it 'should create a BaseDecorator object' do
       decorator_base = BaseDecorator.new('yoda')
       expect(decorator_base).to be_instance_of(BaseDecorator)
       expect(decorator_base.nameable).to eq 'yoda'
     end
-  end  context 'When testing correct_name method' do
+  end
+  context 'When testing correct_name method' do
     it 'should create a DecoratorBase object' do
       decorator_base = BaseDecorator.new('Luke skywalker')
       expect(decorator_base).to be_instance_of(BaseDecorator)
       expect(decorator_base.nameable).to eq 'Luke skywalker'
     end
-  end  let(:mock_nameable) { double('nameable') }
+  end
+  let(:mock_nameable) { double('nameable') }
   subject(:decorator_base) { described_class.new(mock_nameable) }
   context 'when the nameable object returns a correct name' do
     it 'returns the correct name' do
