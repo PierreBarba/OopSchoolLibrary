ae16d9dc9ab88fd4b119380215c5d58e34baee67
Pierre Barba <99061965+PierreBarba@users.noreply.github.com>
2023-03-23 08:24:59 -0600

    test for nameable

---
+++ spec/nameable_spec.rb
@@ -0,0 +1,9 @@
+require_relative '../nameable'
+
+RSpec.describe Nameable do
+    describe '#correct_name' do
+      it 'raises NotImplementedError' do
+        expect { subject.correct_name }.to raise_error NotImplementedError, 'you need to implement the correct_name method by inheritance'
+      end
+    end
+  end
